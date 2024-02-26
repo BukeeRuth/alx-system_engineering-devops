9b9eba317a38fc9fb8dbf18f09abf578bc86d211
BukeeRuth <olatunjibukola018@gmail.com>
2024-02-26 00:25:17 -0800

    site

---
+++ 0x0A-configuration_management/site.pp
@@ -0,0 +1,17 @@
+#!/usr/bin/pup
+
+require 'uri'
+
+class { 'python':
+  version => 'system',
+}
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => '/usr/bin',
+  require => Package['python3-pip'],
+}
